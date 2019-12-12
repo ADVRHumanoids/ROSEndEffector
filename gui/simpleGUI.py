37ab9233743df948e079da9ed70222ff7b5171bd
torydebra <toridebraus@gmail.com>
2019-12-12 16:11:28 +0100

    gui add pinch with selection of (only) 2 finger

--- gui/simpleGUI.py
+++ gui/simpleGUI.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 
 import Tkinter as tk
+import ttk
 import rospy 
 from ros_end_effector.msg import EEGraspControl, EEPinchControl
 
@@ -12,31 +13,44 @@ class Gui:
         
         #windows params and style TODO make it nice 
         winlength = 500
-        winheight = 200
+        winheight = 400
         winposition = "800+500"
         
         self.rootWin = tk.Tk()
         self.rootWin.title("ros_end_effector control GUI")
         self.rootWin.geometry(str(winlength) + "x" + str(winheight) + "+" + winposition)
-        
-        #grid layout for multiple widgets
-        topRow = tk.Frame(self.rootWin)
-        topRow.grid(column=0, row=0)
-        bottomRow = tk.Frame(self.rootWin)
-        bottomRow.grid(column=0, row=1)
+        #self.rootWin.style = ttk.Style() #theme not work
+        #self.rootWin.style.theme_use("classic")  #'aqua', 'step', 'clam', 'alt', 'default', 'classic'
+
+        idxRow = 0
         
         #create the scale widgets
-        #TODO create widgets in a list of objects
-        self.scaleGrasp = GuiScale(topRow, "ros_end_effector/grasp", EEGraspControl)
-        #now only one can be active otherwise they will be in conflict
-        #self.scalePinch = GuiScale(bottomRow, "Open (0) Close(1) PINCH")
+        self.scaleGrasp = GuiScale(self.createFrameinGrid(0,0), 
+                                   "ros_end_effector/grasp", EEGraspControl)
+        
+        
+        #create checkbuttons for the pinch scale
+        #TODO take finger names from urdf
+
+        maxPicks = 2 #only 2 finger for the pinch
+        self.scalePinch = GuiScaleCheck(self.createFrameinGrid(0,1), 
+                                   "ros_end_effector/grasp", EEPinchControl,
+                                   ["finger_1", "finger_2", "finger_3"], maxPicks)
+
+    def createFrameinGrid(self, col, row):
+        """
+        Create a new frame and put it into rootWindow with grid(col, row)
+        """
+        widgetRow = tk.Frame(self.rootWin)
+        widgetRow.grid(column=col, row=row, padx =10, pady=10)
+        return widgetRow
+            
 
         
         
 class GuiScale:
     def __init__(self, frame, topicName, msgType):
         self.value = tk.DoubleVar()
-        self.updated = False
         self.scale = tk.Scale(frame,
             variable = self.value,
             command = self.scaleClbk,
@@ -56,16 +70,95 @@ class GuiScale:
         
         
     def scaleClbk(self, value):
-        #TODO if pinch... different type of msg, switch somewhere?
         msg = self.msgType()
         msg.seq = self.msgSeq
         self.msgSeq += 1
         msg.stamp = rospy.Time.now()
-        print (value)
         msg.percentage = float(value)
         rospy.loginfo("Publishing Grasp message: ")
         rospy.loginfo(msg)
         self.pub.publish(msg)
+            
+
+
+class GuiScaleCheck:
+    """
+    Scale widget associated with some checkbuttons (useful for pinch command)
+    """
+    def __init__(self, frame, topicName, msgType, fingerList, maxPicks):
+        
+        self.selectedFinger = ["", ""]
+        
+        #checkbuttons part
+        self.checkButtons = []
+        self.varCheckButtons = []
+        self.maxPicks = maxPicks
+        
+        for idx, finger in enumerate(fingerList):
+            self.varCheckButtons.append(tk.IntVar())
+            self.checkButtons.append(tk.Checkbutton(frame, 
+                                        text = finger,
+                                        variable = self.varCheckButtons[idx],
+                                        command = self.check_picked))
+            self.checkButtons[idx].grid(row=0, column=idx, pady=10)
+            
+        #scale part
+        self.scaleVar = tk.DoubleVar()
+        self.scale = tk.Scale(frame,
+            variable = self.scaleVar,
+            command = self.scaleClbk,
+            orient = tk.HORIZONTAL, 
+            length = 400,
+            from_ = 0, to = 1,
+            showvalue = 1, 
+            tickinterval = 0.1, resolution = 0.01,
+            label = topicName,
+            state = tk.DISABLED)
+        self.scale.grid(row=1, column=0, columnspan=len(fingerList), padx=10, pady=10)
+        
+        #for ros messages
+        self.msgType = msgType
+        self.pub = rospy.Publisher(topicName, msgType, queue_size=1)
+        self.msgSeq = 0 #sequence incrementing for msg
+        
+    def scaleClbk(self, value):
+        msg = self.msgType()
+        msg.seq = self.msgSeq
+        self.msgSeq += 1
+        msg.stamp = rospy.Time.now()
+        msg.percentage = float(value)
+        msg.finger_pinch_1 = self.selectedFinger[0]
+        msg.finger_pinch_2 = self.selectedFinger[1]
+        rospy.loginfo("Publishing Pinch message: ")
+        rospy.loginfo(msg)
+        self.pub.publish(msg)
+       
+       
+    def check_picked(self) :
+        #each selected checkbutton has a var == 1
+        nPick = sum(var.get() for var in self.varCheckButtons)
+        
+        if self.maxPicks == nPick:
+            nSelect = 0
+            for chk, var in zip(self.checkButtons, self.varCheckButtons):
+                if var.get():
+                    self.selectedFinger[nSelect] = chk['text']
+                    nSelect += 1
+                else: #if zero, button has not been selected, disable it
+                    chk['state'] = tk.DISABLED
+            
+            #enable scale
+            self.scale['state'] = tk.NORMAL
+        
+        else:
+        #enable all buttons and disable scale
+            self.scale['state'] = tk.DISABLED
+
+            for chk in self.checkButtons:
+                chk['state'] = tk.NORMAL
+
+
+
 
 
 if __name__ == "__main__":
