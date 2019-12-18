f2e4e72bb3b3d2c6fb299ff4c685abfc3e834479
torydebra <toridebraus@gmail.com>
2019-12-18 15:49:04 +0100

    moveit collision test first commit

---
+++ src/collisionMoveit.cpp
@@ -0,0 +1,12 @@
+#include <ros/ros.h>
+
+// MoveIt!
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/planning_scene/planning_scene.h>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "collisionTest");
+  
+  
+}
